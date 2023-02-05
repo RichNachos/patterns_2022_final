a4b35bc01f5c7807e3abc6418996e5c38a9c79a4
LukaBrazia <labsa20@freeuni.edu.ge>
2023-02-05 17:14:04 +0400

    Add token creation and validation

---
+++ core/interactors/tokens.py
@@ -0,0 +1,28 @@
+import secrets
+from typing import Protocol
+
+
+class TokenValidator(Protocol):
+    def validate_token(self, token: str) -> bool:
+        pass
+
+
+class TokenProvider(Protocol):
+    def provide_token(self) -> str:
+        pass
+
+
+class RandomHexTokenProvider:
+    def __init__(self, token_len: int):
+        self._token_len = token_len
+
+    def provide_token(self) -> str:
+        return secrets.token_hex(self._token_len)
+
+
+class HardCodedTokenValidator:
+    def __init__(self, token: str):
+        self._token = token
+
+    def validate_token(self, token: str) -> bool:
+        return token == self._token
